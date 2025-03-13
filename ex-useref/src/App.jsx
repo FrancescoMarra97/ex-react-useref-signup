c2344f58072984bf14d765269cf7d8fef755bc65
FrancescoMarra97 <francescomarra192@gmail.com>
2025-03-13 18:34:11 +0100

    Milestone "

--- ex-useref/src/App.jsx
+++ ex-useref/src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 import './App.css'
 
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/~";
 
 function App() {
   const [name, setName] = useState("")
@@ -11,11 +14,36 @@ function App() {
   const [experience, setExperience] = useState("")
   const [description, setDescription] = useState("")
 
+  const isUsernameValid = useMemo(() => {
+    const charsValid = username.split("").every((char) =>
+      letters.includes(char.toLowerCase()) ||
+      numbers.includes(char)
+    )
+    return charsValid && username.length >= 6
 
+  }, [username])
 
+  const isPasswordValid = useMemo(() => {
+    return (
+      password.length >= 8 &&
+      password.split("").some((char) => letters.includes(char)) &&
+      password.split("").some((char) => numbers.includes(char)) &&
+      password.split("").some((char) => symbols.includes(char))
+    )
+  }, [password])
+
+  const isDescriptionValid = useMemo(() => {
+    return description.trim().length < 1000 && description.trim().length > 100
+  })
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (name != "" && name.length > 6 && username != "" && username.length > 6 && password != "" && password.length > 6 && specialization != "" && experience > 0 && description != "" && description.length > 6) {
+    if (name != "" &&
+      name.length > 6 &&
+      isUsernameValid &&
+      isPasswordValid &&
+      specialization != "" &&
+      experience > 0 &&
+      isDescriptionValid) {
       console.log({
         name,
         username,
@@ -58,12 +86,18 @@ function App() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
+          {username.trim() && (
+            <p style={{ color: isUsernameValid ? 'green' : 'red' }}>{isUsernameValid ? "Username Valido" : "Username Non Valido"}</p>
+          )}
 
           <label>Password</label>
           <input type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {password.trim() && (
+            <p style={{ color: isPasswordValid ? 'green' : 'red' }}>{isPasswordValid ? "Password Valida" : "Password Non Valida"}</p>
+          )}
         </div>
 
         <div>
@@ -88,6 +122,9 @@ function App() {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
+        {description.trim() && (
+          <p style={{ color: isDescriptionValid ? 'green' : 'red' }}>{isDescriptionValid ? "Descrizione Valida" : "Descriozione Non Valida"}</p>
+        )}
         <button type='submit' >Submit</button>
       </form>
     </>
